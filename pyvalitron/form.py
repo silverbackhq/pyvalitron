b495656fd2a74ed92e2923e479085c8f2190be91
clivern <hello@clivern.com>
2016-03-25 16:44:23 +0200

    sanitizer and form skelton added

--- pyvalitron/form.py
+++ pyvalitron/form.py
@@ -8,4 +8,32 @@
 """
 
 class Form(object):
-	pass
+
+    _inputs = {}
+    _errors = {}
+    _sinputs = {}
+
+    _validator = None
+    _sanitizer = None
+    _utils = None
+
+    def __init__(self, inputs):
+        self._inputs = inputs
+        self._validator = None
+        self._sanitizer = None
+        self._utils = None
+
+    def get_inputs(self):
+        return self._inputs
+
+    def get_sinputs(self):
+        return self._sinputs
+
+    def get_errors(self):
+        return self._errors
+
+    def validate(self):
+        pass
+
+    def sanitize(self):
+        pass
--- pyvalitron/sanitizer.py
+++ pyvalitron/sanitizer.py
@@ -8,4 +8,24 @@
 """
 
 class Sanitizer(object):
-	pass
+
+    _input = None
+    _sinput = None
+
+    def set_input(self, input_value):
+        self._input = input_value
+
+    def get_sinput(self):
+        return self._sinput
+
+    def get_input(self):
+        return self._input
+
+    def is_exact(self):
+        return self._input == self._sinput and len(self._input) == len(self._sinput)
+
+    def trim(self):
+        pass
+
+    def escape(self):
+        pass
