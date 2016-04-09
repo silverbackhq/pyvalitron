7911a069fb67f372a960acb33bb8a16df33648b9
clivern <hello@clivern.com>
2016-04-09 16:04:17 +0200

    test cases added to all modules

---
+++ tests/test_sanitizer.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+    PyValitron
+    ~~~~~~
+    Python Inputs Validation Library
+    :copyright: (c) 2016 by Clivern (hello@clivern.com).
+    :license: MIT, see LICENSE for more details.
+"""
+from __future__ import print_function
+from pyvalitron.sanitizer import Sanitizer
+import unittest
+
+
+class TestSanitizerMethods(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
