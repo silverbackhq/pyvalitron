a6ab616c4ab582b0abc77d7ff2e182422732032f
clivern <hello@clivern.com>
2016-04-10 18:07:14 +0200

    form and extensions examples

---
+++ examples/extensions_example.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+    PyValitron
+    ~~~~~~
+    Python Inputs Validation Library
+    :copyright: (c) 2016 by Clivern (hello@clivern.com).
+    :license: MIT, see LICENSE for more details.
+"""
